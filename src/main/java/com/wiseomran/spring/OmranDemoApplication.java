f63bd7ddf68012ef9ec95f3998bf097df31581c3
Mohamed Omran <Mohamed Omran>
2016-05-14 23:31:54 +0300

    making spring jar not war

--- src/main/java/com/wiseomran/spring/OmranDemoApplication.java
+++ src/main/java/com/wiseomran/spring/OmranDemoApplication.java
@@ -1,12 +1,27 @@
 package com.wiseomran.spring;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+@RestController
 public class OmranDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OmranDemoApplication.class, args);
 	}
+	
+	@RequestMapping("/{name}")
+	public String sayHello (@PathVariable String name){
+		return "Hello, "+ name + "!";
+ 	}
 }
