0df7c218d2cce81e9901c4101dc8a25decc52aeb
Mohamed Omran <Mohamed Omran>
2016-05-14 14:04:24 +0300

    first spring commit

---
+++ src/main/java/com/wiseomran/spring/OmranDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wiseomran.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OmranDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OmranDemoApplication.class, args);
+	}
+}
---
+++ src/test/java/com/wiseomran/spring/OmranDemoApplicationTests.java
@@ -0,0 +1,18 @@
+package com.wiseomran.spring;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OmranDemoApplication.class)
+@WebAppConfiguration
+public class OmranDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
